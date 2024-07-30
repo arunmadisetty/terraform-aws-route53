b2c2376b6cbda68533bcb40a202b6bd809846a2f
arunmadisetty <arun826@live.com>
2024-07-29 19:52:04 -0500

    resource creation

---
+++ variables.tf
@@ -0,0 +1,17 @@
+variable "name" {
+  type = string
+  default = ""
+  description = "Please replace default with domain name"
+}
+
+variable "type" {
+  type = string
+  default = ""
+  description = "Please replace default with record type"
+}
+
+variable "env" {
+  type = string
+  default = ""
+  description = "Please replace default with subdomain name"
+}
